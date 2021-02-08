3130051e4c730122b951b6a3f9635f7d5127db1d
jgosar <36840705+jgosar@users.noreply.github.com>
2021-02-08 11:04:18 +0100

    Added styles for game-chooser

--- angular-connect-four/src/app/components/game-chooser/game-chooser.component.config.ts
+++ angular-connect-four/src/app/components/game-chooser/game-chooser.component.config.ts
@@ -1,10 +1,12 @@
 export const GAMES = [
   {
     name: 'Connect 4',
-    url: '/connect-4'
+    url: '/connect-4',
+    thumbnail: '../../../assets/icons/connect-4.png'
   },
   {
     name: 'Checkers',
-    url: '/checkers'
+    url: '/checkers',
+    thumbnail: '../../../assets/icons/checkers.png'
   }
 ];
